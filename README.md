bd12a4c2bf83cba02ac7d903b2c4abaa6274a403
Ali Labbene <ali.labbene@st.com>
2025-07-23 16:51:14 +0100

    Initial version of the very first ST Zephyr module

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# zephyr-stm32-mw-isp
+# Zephyr STM32 Image Signal Processing (ISP) Module
+
+This module provides the necessary code and libraries to enable Image Signal Processing (ISP) support for Zephyr RTOS. It is compatible with the Zephyr STM32 DCMIPP driver.
+
+## Module Components
+
+The module consists of the following components:
+
+1. **ISP Middleware Stack**
+   Located in the `stm32-mw-isp` directory, this stack provides the core ISP functionality.
+   > **Note:** This code is provided as a submodule. Ensure that you include `submodules: true` in your `west.cfg` manifest file.
+
+2. **Glue Code**
+   Found in the `isp_wrapper` directory, this code bridges the Zephyr DCMIPP driver and the ISP middleware stack.
+
+## How to Use the Module
+
+### 1. Add the Module to Your West Manifest
+
+Include the following lines in the `projects` section of your `west.yml` manifest file. Adjust the `name`, `path`, and `revision` fields as needed for your project:
+
+```yaml
+    - name: zephyr-stm32-mw-isp
+      url: https://github.com/stm32-hotspot/zephyr-stm32-mw-isp.git
+      revision: main
+      path: st/lib/zephyr-stm32-mw-isp
+      submodules: true
+```
+
+### 2. Enable the Module in Your Project Configuration
+
+Add the following line to your project's `prj.conf` file to enable the ISP module:
+
+```
+CONFIG_USE_STM32_MW_ISP=y
+```
+
+### 3. Build and Run
+
+After completing the above steps, build your Zephyr project as usual. The ISP module will be included and configured automatically.
+For instance, to run `samples/drivers/video/capture` sample on `stm32n6570_dk`, use the following:
+```
+west build -p always -b stm32n6570_dk/stm32n657xx --sysbuild samples/drivers/video/capture -- -DSHIELD=st_b_cams_imx_mb1854
+west flash
+```
+
+## Additional Notes
+
+- Ensure that the `stm32-mw-isp` submodule is initialized and updated by running the following command in your project directory:
+  ```
+  west update
+  ```
+- For detailed documentation on the ISP middleware stack, refer to the `stm32-mw-isp` directory.
+
+## Compatibility
+
+This module is designed to work seamlessly with the STM32 DCMIPP driver in Zephyr RTOS starting [release v4.2.0](https://github.com/zephyrproject-rtos/zephyr/tree/v4.2.0).
+Ensure that your hardware and software configurations are compatible with the STM32 DCMIPP driver requirements.
+
+---
---
+++ isp_wrapper/src/isp_wrapper.c
@@ -0,0 +1,203 @@
+/**
+ ******************************************************************************
+ * @file    isp_wrapper.c
+ * @author  GPM Application Team
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include "stm32n6xx_hal.h"
+
+#include <assert.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+
+#include "isp_api.h"
+
+#include "imx335_isp_param_conf.h"
+#include "zephyr/drivers/video-controls.h"
+#include "zephyr/drivers/video.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(isp_wrapper);
+
+extern void stm32_dcmipp_isp_vsync_update(DCMIPP_HandleTypeDef *hdcmipp, uint32_t Pipe);
+extern int stm32_dcmipp_isp_init(DCMIPP_HandleTypeDef *hdcmipp, const struct device *sensor);
+extern int stm32_dcmipp_isp_start(void);
+extern int stm32_dcmipp_isp_stop(void);
+
+static const struct device *sensor_i;
+static ISP_HandleTypeDef isp_i;
+static struct k_thread isp_thread;
+static K_THREAD_STACK_DEFINE(isp_thread_stack, 4096);
+static struct k_sem isp_sem;
+
+#define IMX335_BAYER_PATTERN      0
+#define IMX335_COLOR_DEPTH        10
+#define IMX335_WIDTH              2592
+#define IMX335_HEIGHT             1944
+#define IMX335_GAIN_MIN           (0 * 1000)
+#define IMX335_GAIN_MAX           (72 * 1000)
+#define IMX335_EXPOSURE_MIN       8
+#define IMX335_EXPOSURE_MAX       33266
+
+#define IMX335_1H_PERIOD_USEC (1000000.0F / 4500 / 30)
+
+static ISP_StatusTypeDef isp_GetSensorInfo(uint32_t Instance, ISP_SensorInfoTypeDef *info)
+{
+  info->bayer_pattern = IMX335_BAYER_PATTERN;
+  info->color_depth = IMX335_COLOR_DEPTH;
+  info->width = IMX335_WIDTH;
+  info->height = IMX335_HEIGHT;
+  info->gain_min = IMX335_GAIN_MIN;
+  info->gain_max = IMX335_GAIN_MAX;
+  info->exposure_min = IMX335_EXPOSURE_MIN;
+  info->exposure_max = IMX335_EXPOSURE_MAX;
+
+  return 0;
+}
+
+static ISP_StatusTypeDef isp_SetSensorGain(uint32_t Instance, int32_t Gain)
+{
+  struct video_control ctrl;
+  int ret;
+
+  assert(sensor_i);
+
+  ctrl.id = VIDEO_CID_ANALOGUE_GAIN;
+  ctrl.val = Gain;
+  ret = video_set_ctrl(sensor_i, &ctrl);
+
+  return ret;
+}
+
+static ISP_StatusTypeDef isp_GetSensorGain(uint32_t Instance, int32_t *Gain)
+{
+  struct video_control ctrl;
+  int ret;
+
+  ctrl.id = VIDEO_CID_ANALOGUE_GAIN;
+  ret = video_get_ctrl(sensor_i, &ctrl);
+  if (ret)
+    return ret;
+
+  *Gain = ctrl.val;
+
+  return 0;
+}
+
+static ISP_StatusTypeDef isp_SetSensorExposure(uint32_t Instance, int32_t Exposure)
+{
+  struct video_control ctrl;
+  int ret;
+
+  assert(sensor_i);
+
+  ctrl.id = VIDEO_CID_EXPOSURE;
+  ctrl.val = Exposure / IMX335_1H_PERIOD_USEC;
+  ret = video_set_ctrl(sensor_i, &ctrl);
+
+  return ret;
+}
+
+static ISP_StatusTypeDef isp_GetSensorExposure(uint32_t Instance, int32_t *Exposure)
+{
+  struct video_control ctrl;
+  int ret;
+
+  ctrl.id = VIDEO_CID_EXPOSURE;
+  ret = video_get_ctrl(sensor_i, &ctrl);
+  if (ret)
+    return ret;
+
+  *Exposure = ctrl.val * IMX335_1H_PERIOD_USEC;
+
+  return 0;
+}
+
+static ISP_AppliHelpersTypeDef isp_helpers = {
+  .GetSensorInfo = isp_GetSensorInfo,
+  .SetSensorGain = isp_SetSensorGain,
+  .GetSensorGain = isp_GetSensorGain,
+  .SetSensorExposure = isp_SetSensorExposure,
+  .GetSensorExposure = isp_GetSensorExposure,
+};
+
+static void isp_thread_ep(void *arg1, void *arg2, void *arg3)
+{
+  int res;
+
+  while (1) {
+    res = k_sem_take(&isp_sem, K_FOREVER);
+    assert(res == 0);
+
+    //LOG_INF("Run isp update");
+    res = ISP_BackgroundProcess(&isp_i);
+    assert(res == 0);
+  }
+}
+
+int stm32_dcmipp_isp_init(DCMIPP_HandleTypeDef *hdcmipp, const struct device *sensor)
+{
+  k_tid_t isp_tid;
+  int res;
+
+  sensor_i = sensor;
+  res = ISP_Init(&isp_i, hdcmipp, 0, &isp_helpers, &ISP_IQParamCacheInit_IMX335);
+  if (res)
+    return -res;
+
+  res = k_sem_init(&isp_sem, 0, 1);
+  if (res)
+    return -res;
+
+  isp_tid = k_thread_create(&isp_thread, isp_thread_stack, K_THREAD_STACK_SIZEOF(isp_thread_stack), isp_thread_ep,
+         NULL, NULL, NULL, 0, 0, K_NO_WAIT);
+  if (!isp_tid)
+    return -1;
+
+  return 0;
+}
+
+int stm32_dcmipp_isp_start(void)
+{
+  ISP_StatusTypeDef res;
+
+  res = ISP_Start(&isp_i);
+
+  return res;
+}
+
+int stm32_dcmipp_isp_stop(void)
+{
+  assert(0);
+
+  return 0;
+}
+
+void stm32_dcmipp_isp_vsync_update(DCMIPP_HandleTypeDef *hdcmipp, uint32_t Pipe)
+{
+  if (Pipe == 0) {
+    ISP_IncDumpFrameId(&isp_i);
+  } else if (Pipe == 1) {
+    ISP_IncMainFrameId(&isp_i);
+    ISP_GatherStatistics(&isp_i);
+  } else if (Pipe == 2) {
+    ISP_IncAncillaryFrameId(&isp_i);
+  }
+
+  if (Pipe != 1)
+    return ;
+
+  k_sem_give(&isp_sem);
+}
